7b700d9684ac16d6460b12b9b6c271b53eaf9436
thisisfunt <64325631+thisisfunt@users.noreply.github.com>
2020-05-13 16:22:07 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+import telebot
+import random
+
+bot = telebot.TeleBot("1203250218:AAE6415lw9GPMNFDkCtH4xwt0mg94dqckDo")
+s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Приветствую, друг!\nЯ бот который поможет тебе создать пароль, который нельзя взломать.\nЧтобы создать напиши мне make (длина пароля)")
+
+@bot.message_handler(content_types=["text"])
+def send_info(message):
+    if(message.text.split(" ")[0] == "make"):
+        lenght = int(message.text.split(" ")[1])
+        password = ""
+        i = 0
+        while(i < lenght):
+            password += s[random.randint(0, len(s) - 1)]
+            i += 1
+        bot.send_message(message.chat.id,"Держи твой пароль: " + password)
+
+bot.polling(none_stop=True)
